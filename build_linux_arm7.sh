f73d7b50c353be0780e6336c5d72562bb06f0611
leruetkins <leruetkins@gmail.com>
2023-07-20 18:49:39 +0300

    Update build_linux_arm7.sh

--- build_linux_arm7.sh
+++ build_linux_arm7.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-RUSTFLAGS="-C linker=arm-linux-gnueabihf-gcc --verbose" cargo build --release --target armv7-unknown-linux-gnueabihf
+VERSION=$(grep '^version =' Cargo.toml | cut -d '=' -f2 | tr -d '"')
+echo $VERSION
+
+RUSTFLAGS="-C linker=arm-linux-gnueabihf-gcc --verbose" cargo build --release --target armv7-unknown-linux-gnueabihf -o ka-server-arm-$VERSION.bin
